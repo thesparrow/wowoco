f86ebde8243add6e4e802727bf7c94be40d77399
nikkisarat2000 <nikkisarat2000@hotmail.com>
2018-03-03 20:20:39 -0800

    boostrap4 mobile chart image

--- ecard/Pages/Forms.cshtml.cs
+++ ecard/Pages/Forms.cshtml.cs
@@ -2,13 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ecard.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace ecard.Pages
 {
 	public class FormsModel : PageModel
 	{
+		//[BindProperty]
+		//public Greetings BridgeGreetings { get; set; }
+		//private IConfiguration MyConfiguration { get; set; }
+		//// CONNECT TO MY DATABASE
+		//private myDB _myDB;
+		//public FormsModel(myDB myDB, IConfiguration MyConfiguration)
+		//{
+		//}
+
 		public void OnGet()
 		{
 
